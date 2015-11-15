95f83346dbec798161c146ff153a5d2dc3234a06
Takuya-f <fukamachi@posl.ait.kyushu-u.ac.jp>
2015-11-16 04:46:52 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
+public class Main extends JFrame implements ActionListener{
+
+	private static final long serialVersionUID = 1L;
+	private Student[] studentData = {
+			new Student("1","Taro",true),
+			new Student("2","Jiro",true),
+			new Student("3","Saburo",false),
+			new Student("4","Hanako",true),
+			new Student("5","Yumi",false),
+			new Student("6","Emi",true)
+	};
+	private String[] columnName = { "ID", "Name", "Class" };
+	private Object[][] tableData;
+	public static Main mainFrame;
+
+	public Main() {
+		setTitle("StudentList");
+		setVisible(true);
+		setBounds(0, 0, 320, 240);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		this.tableData = new Object[studentData.length][];
+		for(int i = 0; i < studentData.length; i++){
+			tableData[i] = studentData[i].getTableData();
+		}
+
+		JTable table = new JTable(this.tableData, this.columnName);
+
+		JButton button = new JButton("授業取得者のみに絞り込む");
+		button.addActionListener(this);
+
+		JPanel p = new JPanel();
+		p.add(table.getTableHeader(), BorderLayout.PAGE_START);
+		p.add(table);
+		p.add(button);
+
+		getContentPane().add(p, BorderLayout.CENTER);
+
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		mainFrame = new Main();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		Container container = mainFrame.getContentPane();
+		JTable table = (JTable) ((JPanel)container.getComponent(0)).getComponent(1);
+		// filter Students
+	}
+
+}
